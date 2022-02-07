1da2086b39476c7d2af255b8fa4f27fec4673fe4
Pankaj Mishra <pankaj.mishra@extentia.com>
2022-02-07 08:11:05 +0000

    Enable Third Column Pages

--- sal_hr/webapp/controller/DetailDetailPage.controller.js
+++ sal_hr/webapp/controller/DetailDetailPage.controller.js
@@ -1,10 +1,44 @@
-sap.ui.define([ 
+sap.ui.define([
     "./BaseController",
     "sap/ui/core/mvc/Controller"
 ],
 
     function (BaseController, Controller) {
         "use strict";
-        return BaseController.extend("com.sal.salhr.controller.DetailDetailPage", {});
+        return BaseController.extend("com.sal.salhr.controller.DetailDetailPage", {
+            onInit: function () {
+
+                this.oRouter = this.getRouter();
+                this.oRouter.getRoute("detailDetail").attachPatternMatched(this._onObjectMatched, this);
+
+            },
+            _onObjectMatched: function (oEvent) {
+                debugger;
+                this.sParentID = oEvent.getParameter("arguments").parentMaterial;
+                var sLayout = oEvent.getParameter("arguments").layout;
+                // if (sLayout == 'TwoColumnsMidExpanded') {
+                //     this.byId("idViewBOQListButton").setPressed(false);
+                //     this.getViewModel("objectViewModel").setProperty("/sViewBOQButtonName", "View BOQ List");
+                // }
+                // this.getView().getModel().setProperty("/busy", false);
+                this.getView().getModel("layoutModel").setProperty("/layout", sLayout);
+                this._bindView();
+                // this._filterPCListTable(this.UserEmail);
+            },
+
+            _bindView: function () {
+                var objectViewModel = this.getViewModel("objectViewModel");
+                var that = this;
+                var oComponentModel = this.getComponentModel();
+                //    var sTickets = sObjectPath + "/tickets";
+                var sKey = oComponentModel.createKey("/MasterSubModules", {
+                    ID: this.sParentID
+                });
+
+                this.getView().bindElement({
+                    path: sKey
+                });
+            }
+        });
 
     });
