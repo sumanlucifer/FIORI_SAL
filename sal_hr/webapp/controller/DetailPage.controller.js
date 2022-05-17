048ed9140bb76bb2086ae888e9a55d07e01aecb8
Manasa GP <manasa.gandhalu@extentia.com>
2022-05-17 13:55:22 +0000

    updated code

--- sal_hr/webapp/controller/DetailPage.controller.js
+++ sal_hr/webapp/controller/DetailPage.controller.js
@@ -185,8 +185,13 @@ sap.ui.define([
             onPressTicketItem: function (oEvent) {
                 var sStatus = oEvent.getSource().getBindingContext().getObject().status;
 
-                if(sStatus === "APPROVED" || sStatus === "PENDING" || sStatus=== "REJECTED")
-                {
+                if(sStatus==='CANCELLED'){
+
+                    sap.m.MessageToast.show("The record is removed from the source system.")
+                    return;
+                }
+
+               
                     switch (this.sParentID) {
            
                     
@@ -299,9 +304,7 @@ sap.ui.define([
                             break;
                     }
                 }
-                else{
-                    sap.m.MessageBox.error("This record is removed from the source system");
-                }
+               
 
            
 
