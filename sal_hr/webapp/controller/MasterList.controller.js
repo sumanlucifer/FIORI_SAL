b38dacb1a3e1a49e13b5896f34a1c9aad7365f1e
Snehal Wadhane <snehal.wadhane@extentia.com>
2022-06-06 11:07:26 +0000

    Role based Access changes

--- sal_hr/webapp/controller/MasterList.controller.js
+++ sal_hr/webapp/controller/MasterList.controller.js
@@ -3,11 +3,11 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "com/sal/salhr/model/formatter",
     "sap/ui/model/Filter",
-	"sap/ui/model/FilterOperator",
+    "sap/ui/model/FilterOperator",
     'sap/ui/model/Sorter'
 ],
-   
-    function (BaseController,Controller,formatter,Filter,FilterOperator,Sorter) {
+
+    function (BaseController, Controller, formatter, Filter, FilterOperator, Sorter) {
         "use strict";
 
         return BaseController.extend("com.sal.salhr.controller.MasterList", {
@@ -24,22 +24,21 @@ sap.ui.define([
             _onObjectMatched: function (oEvent) {
 
                 var params = new URLSearchParams(decodeURIComponent(window.parent.location.href)),
-                subModuleId = params.get("submoduleId"),
-                ticketId = params.get("ticketId");
-                if(subModuleId && ticketId)
-                {
-                this._navToDetail(subModuleId);
+                    subModuleId = params.get("submoduleId"),
+                    ticketId = params.get("ticketId");
+                if (subModuleId && ticketId) {
+                    this._navToDetail(subModuleId);
                 }
                 var sLayout = oEvent.getParameter("arguments").layout;
                 this.getView().getModel("layoutModel").setProperty("/layout", sLayout);
-            },   
-            onUpdateMasterListBindingStart:function(oEvent){
+            },
+            onUpdateMasterListBindingStart: function (oEvent) {
                 var sIsUserManager = this.getOwnerComponent().getModel("EmpInfoModel").getProperty("/IsUserManager").toString();
                 oEvent.getSource().getBinding("items").sCustomParams = "IsUserManager=" + sIsUserManager;
                 oEvent.getSource().getBinding("items").mCustomParams.IsUserManager = sIsUserManager;
             },
             _navToDetail: function (id) {
-               
+
                 this.getRouter().navTo("detail", {
                     parentMaterial: id,
                     layout: "TwoColumnsMidExpanded"
@@ -48,75 +47,66 @@ sap.ui.define([
                 );
 
 
-                
-            },
-            
-            
 
-           
-             onMasterListPress: function (oEvent) {          
+            },
+
+
+
+
+            onMasterListPress: function (oEvent) {
                 this._showObject(oEvent.getSource());
             },
 
-           
+
             _showObject: function (oItem) {
                 var that = this;
                 var sManagerTile = this.getView().getModel("EmpInfoModel").getData().IsUserManager;
                 var subModuleId = oItem.getBindingContext().getObject().ID;
-                this.fnGetRoleAccess(sManagerTile,subModuleId);
-
-                var sPath = oItem.getBindingContextPath()
-                this.getRouter().navTo("detail", {
-                    parentMaterial: oItem.getModel().getProperty(sPath).ID,
-                    layout: "TwoColumnsMidExpanded"
-                },
-                    false
-                );
+                this.fnGetRoleAccess(sManagerTile, subModuleId, oItem);
             },
-            onSearch:function(oEvent){
+            onSearch: function (oEvent) {
                 var aFilters = [];
                 var sQuery = oEvent.getSource().getValue();
                 if (sQuery && sQuery.length > 0) {
-                    
-                    aFilters.push( this.createFilter("name", FilterOperator.Contains, sQuery, true) );
-                   
+
+                    aFilters.push(this.createFilter("name", FilterOperator.Contains, sQuery, true));
+
                 }
-    
-              
+
+
                 var oTable = this.byId("idMasterTable");
                 var oBinding = oTable.getBinding("items");
                 oBinding.filter(aFilters);
 
             },
-            createFilter: function(key, operator, value, useToLower) {
+            createFilter: function (key, operator, value, useToLower) {
                 return new Filter(useToLower ? "tolower(" + key + ")" : key, operator, useToLower ? "'" + value.toLowerCase() + "'" : value);
             },
             onSort: function () {
                 this._bDescendingSort = !this._bDescendingSort;
                 var oBinding = this.getView().byId("idMasterTable").getBinding("items"),
                     oSorter = new Sorter("name", this._bDescendingSort);
-    
+
                 oBinding.sort(oSorter);
             },
-            fnGetRoleAccess: function (sManagerTile,subModuleId) {
-                debugger;
-            
+
+            fnGetRoleAccess: function (sManagerTile, subModuleId, oItem) {
                 this.sManagerTile = sManagerTile;
                 var oComponentModel = this.getComponentModel();
                 oComponentModel.read("/MasterRolePermission", {
-                urlParameters: {
-                    "subModuleId": subModuleId
-                }, 
-                
+                    urlParameters: {
+                        "subModuleId": subModuleId
+                    },
+
                     success: function (oData) {
-                        
-                        if(this.sManagerTile === false){
+
+                        if (this.sManagerTile === false) {
                             this.getOwnerComponent().getModel("RoleInfoModel").setProperty("/createSelf", oData.results[0].createSelf);
                             this.getOwnerComponent().getModel("RoleInfoModel").setProperty("/deleteSelf", oData.results[0].deleteSelf);
                             this.getOwnerComponent().getModel("RoleInfoModel").setProperty("/readSelf", oData.results[0].readSelf);
                             this.getOwnerComponent().getModel("RoleInfoModel").setProperty("/updateSelf", oData.results[0].updateSelf);
-                        }else{
-    
+                        } else {
+
                             this.getOwnerComponent().getModel("RoleInfoModel").setProperty("/approveOther", oData.results[0].approveOther);
                             this.getOwnerComponent().getModel("RoleInfoModel").setProperty("/createOther", oData.results[0].createOther);
                             this.getOwnerComponent().getModel("RoleInfoModel").setProperty("/deleteOther", oData.results[0].deleteOther);
@@ -124,14 +114,18 @@ sap.ui.define([
                             this.getOwnerComponent().getModel("RoleInfoModel").setProperty("/rejectOther", oData.results[0].rejectOther);
                             this.getOwnerComponent().getModel("RoleInfoModel").setProperty("/updateOther", oData.results[0].updateOther);
                         }
-                       
-                        
+                        var sPath = oItem.getBindingContextPath()
+                        this.getRouter().navTo("detail", {
+                            parentMaterial: oItem.getModel().getProperty(sPath).ID,
+                            layout: "TwoColumnsMidExpanded"
+                        },
+                            false
+                        );
                     }.bind(this),
                     error: function (oError) {
                         sap.m.MessageBox.error(JSON.stringify(oError));
                     }.bind(this),
                 });
             }
-
         });
     });
