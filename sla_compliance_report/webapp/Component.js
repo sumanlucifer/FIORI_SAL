57f04573e3c0ccf4e10989dbc7658a845acff57c
Manasa GP <manasa.gandhalu@extentia.com>
2022-06-27 11:48:52 +0000

    Reports Fix

--- sla_compliance_report/webapp/Component.js
+++ sla_compliance_report/webapp/Component.js
@@ -1,9 +1,11 @@
 sap.ui.define([
         "sap/ui/core/UIComponent",
         "sap/ui/Device",
-        "com/sal/reports/slacompliancereport/model/models"
+        "com/sal/reports/slacompliancereport/model/models",
+        'sap/ui/model/json/JSONModel'
+        
     ],
-    function (UIComponent, Device, models) {
+    function (UIComponent, Device, models, JSONModel) {
         "use strict";
 
         return UIComponent.extend("com.sal.reports.slacompliancereport.Component", {
@@ -25,6 +27,16 @@ sap.ui.define([
 
                 // set the device model
                 this.setModel(models.createDeviceModel(), "device");
+
+                 //this will return the semantic object and action alongwith the routing params
+                 var oHashObjectPath = new sap.ui.core.routing.HashChanger().getHash(),
+                 bIsUserManager = false;
+                if (oHashObjectPath.indexOf("Manage") > 5 && oHashObjectPath.indexOf("Manage") < 20) {
+                 bIsUserManager = true;
+                 }
+
+             this.setModel(new JSONModel(), "EmpInfoModel");
+             this.getModel("EmpInfoModel").setProperty("/IsUserManager", bIsUserManager);
             }
         });
     }
