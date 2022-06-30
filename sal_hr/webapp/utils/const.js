2505ea58c970248617bb57164d8d65c59b92caa5
Manasa GP <manasa.gandhalu@extentia.com>
2022-06-30 18:14:58 +0000

    Redirection changes

---
+++ sal_hr/webapp/utils/const.js
@@ -0,0 +1,8 @@
+sap.ui.define([], function () {
+    "use strict";
+    return {
+        LINKS: {
+            SFSF: "https://performancemanager.successfactors.eu/sf/liveprofile?_s.crb=GCk4JxCWiKUDD08a2WKBhKkyS0meHIE3sUS2RCjkXC4%3d"
+        }
+    }
+}); 
