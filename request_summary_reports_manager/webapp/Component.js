54339654f1e3fdc109321ebb29b475e21ad78f91
Ankit Pundhir <ankit.pundhir@extentia.com>
2022-07-08 13:15:02 +0000

    fixed issue related with timezone date format

--- request_summary_reports_manager/webapp/Component.js
+++ request_summary_reports_manager/webapp/Component.js
@@ -6,6 +6,16 @@ sap.ui.define(
         return Component.extend("com.sal.requestsummaryreportsmanager.Component", {
             metadata: {
                 manifest: "json"
+            },
+
+            onBeforeRendering: function() {
+                let modelRead = this.getModel().read;
+                this.getModel().read = (url, config) => {
+                    console.log(url, config);
+                    const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+                    config.urlParameters.push(`timezone=${timezone}`);
+                    return modelRead.apply(this.getModel(), [url, config]);
+                };
             }
         });
     }
