7cbecc8998bc456e33acd6ec2d3de2bbd4514a38
SUMAN SHANMUGAM <suman.shanmugam@extentia.com>
2022-08-22 16:54:18 +0000

    feedback form changes

--- feedbackform/webapp/Component.js
+++ feedbackform/webapp/Component.js
@@ -1,9 +1,10 @@
 sap.ui.define([
         "sap/ui/core/UIComponent",
         "sap/ui/Device",
-        "com/sal/feedbackform/model/models"
+        "com/sal/feedbackform/model/models",
+        'sap/ui/model/json/JSONModel'
     ],
-    function (UIComponent, Device, models) {
+    function (UIComponent, Device, models, JSONModel) {
         "use strict";
 
         return UIComponent.extend("com.sal.feedbackform.Component", {
@@ -25,6 +26,44 @@ sap.ui.define([
 
                 // set the device model
                 this.setModel(models.createDeviceModel(), "device");
+              
+
+                //this will return the semantic object and action alongwith the routing params
+                var oHashObjectPath = new sap.ui.core.routing.HashChanger().getHash(),
+                    bIsUserManager = false;
+
+                if (oHashObjectPath.indexOf("Manage") > 5 && oHashObjectPath.indexOf("Manage") < 20) {
+                    bIsUserManager = true;
+                }
+
+                this.setModel(new JSONModel(), "EmpInfoModel");
+                this.getModel("EmpInfoModel").setProperty("/IsUserManager", bIsUserManager);
+
+                this.setModel(new JSONModel(), "RoleInfoModel");
+
+                // Set the user model
+                this.fnGetLoggedInEmpInfo(bIsUserManager);
+            },
+            fnGetLoggedInEmpInfo: function (bIsUserManager) {
+            
+                
+              
+                this.getModel().read("/EmpInfo", {
+                    urlParameters: {
+                        "moreInfo": "true",
+                        // "$filter": "userId eq '12000843'"
+                    },
+                    success: function (oData) {        
+                        this.getRouter().navTo("master");       
+                        this.setModel(new JSONModel(oData.results[0]), "EmpInfoModel");
+                        this.getModel("EmpInfoModel").setProperty("/IsUserManager", bIsUserManager);
+                       
+                    }.bind(this),
+                    error: function (oError) {   
+                         
+                        sap.m.MessageBox.error(JSON.stringify(oError));
+                    }.bind(this),
+                });
             }
         });
     }
--- feedbackform/webapp/controller/feedbackform.controller.js
+++ feedbackform/webapp/controller/feedbackform.controller.js
@@ -12,6 +12,10 @@ sap.ui.define(
         },
 
         onSubmitPress: function () {
+
+
+          
+
           // if (!this._validateMandatoryFields()) {
 
           //     return;
@@ -22,48 +26,79 @@ sap.ui.define(
             sSuggestionEntityPath = "/Suggestion";
 
           this.getView().setBusy(true);
+          Promise.allSettled([
 
-          this.mainModel.create(sRatingEntityPath, oRatingPayloadObj, {
-            success: function (oData, oResponse) {
-              sap.m.MessageBox.success("Request Submitted Successfully.");
-              this.getView().setBusy(false);
-              this.getView().getModel().refresh();
-              this.oRouter.navTo("detail", {
-                parentMaterial: this.sParentID,
-                layout: "TwoColumnsMidExpanded",
-              });
-            }.bind(this),
-            error: function (oError) {
-              this.getView().setBusy(false);
-              sap.m.MessageBox.error(
-                JSON.parse(
-                  JSON.parse(oError.responseText).error.message.value
-                ).error.message.value.split("]")[1]
-              );
-              this.getView().getModel().refresh();
-            }.bind(this),
-          });
+            this.callAPIServiceInstance(sRatingEntityPath, oRatingPayloadObj),
+            this.callAPIServiceInstance(sSuggestionEntityPath, oSuggestionPayloadObj)
+        ]).then(this.PromiseResponse.bind(this)).catch(function (error) {}.bind(this));
+
+        //   this.mainModel.create(sRatingEntityPath, oRatingPayloadObj, {
+        //     success: function (oData, oResponse) {
+        //       sap.m.MessageBox.success("Thank You for Your Feedback.");
+        //       this.getView().setBusy(false);
+        //       this.getView().getModel().refresh();
+        //       this.oRouter.navTo("detail", {
+        //         parentMaterial: this.sParentID,
+        //         layout: "TwoColumnsMidExpanded",
+        //       });
+        //     }.bind(this),
+        //     error: function (oError) {
+        //       this.getView().setBusy(false);
+        //       sap.m.MessageBox.error(
+        //         JSON.parse(
+        //           JSON.parse(oError.responseText).error.message.value
+        //         ).error.message.value.split("]")[1]
+        //       );
+        //       this.getView().getModel().refresh();
+        //     }.bind(this),
+        //   });
         },
 
+
+        callAPIServiceInstance: function (path, oPayload) {
+
+			return new Promise(
+				function (resolve, reject) {
+                    this.mainModel.create(path, oPayload, {
+						success: function (oData) {
+							resolve(oData);
+						},
+						error: function (oResult) {
+							reject(oResult);
+
+						}
+					});
+				}.bind(this));
+		},
+        PromiseResponse : function(aValues)
+        {
+            this.getView().setBusy(false);
+            sap.m.MessageBox.success("Thank You for Your Feedback.");
+            this.getView().byId("idRating").setSelectedIndex(null);
+            this.getView().byId("idSuggestion").setValue("");
+        },
         fnGetRatingPayload: function () {
+         var  sEmployeeID = this.getOwnerComponent().getModel("EmpInfoModel").getData().userId;
+
           return {
-            employeeId: 12002024,
+            employeeId: sEmployeeID,
             moduleId: 1,
             masterSubModuleId: 60,
             rating: this.getView().byId("idRating").getSelectedIndex() === 0 ? 1 : this.getView().byId("idRating").getSelectedIndex(),
-            questionId: 2,
-            Source : "EP_Portlet"
+            questionId: 2
+         
           };
         },
 
         fnGetSuggestionPayload: function () {
+            var  sEmployeeID = this.getOwnerComponent().getModel("EmpInfoModel").getData().userId;
           return {
-            employeeId: 11,
+            employeeId: sEmployeeID,
             moduleId: 4,
             masterSubModuleId: 2,
             questionId: 3,
-            suggestion: this.getView().byId("idSuggestion").getValue(),
-            Source : "EP_Portlet"
+            suggestion: this.getView().byId("idSuggestion").getValue()
+          
           };
         },
       }
