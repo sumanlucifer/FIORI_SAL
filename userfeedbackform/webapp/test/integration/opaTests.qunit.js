781f128aa36f93db17ead9219e61caab1514da85
SUMAN SHANMUGAM <suman.shanmugam@extentia.com>
2022-08-22 17:26:30 +0000

    test commit

---
+++ userfeedbackform/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/sal/userfeedbackform/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ userfeedbackform/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comsal/userfeedbackform/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
