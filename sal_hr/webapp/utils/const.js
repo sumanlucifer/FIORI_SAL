1100312be796e377ae4c96d5d895f124fbd1400a
SUMAN SHANMUGAM <suman.shanmugam@extentia.com>
2022-09-02 10:59:30 +0000

    feedbac report UI cosmetic fix

--- sal_hr/webapp/utils/const.js
+++ sal_hr/webapp/utils/const.js
@@ -3,21 +3,21 @@ sap.ui.define([], function () {
     return {
         LINKS: {
             // **************** DEV URL ****************************************
-            Vacancy: "https://performancemanager.successfactors.eu/sf/careers/jobsearch?bplte_company=saudiairliD&_s.crb=3LsYZ05MZQCjDzCRxMwWTzwtWvMBA3IqTNRLTgCgQn4%253d",
-            Learning: "https://performancemanager.successfactors.eu/sf/careers/jobsearch?bplte_company=saudiairliD&_s.crb=3LsYZ05MZQCjDzCRxMwWTzwtWvMBA3IqTNRLTgCgQn4%253d",
-            Loan: "https://performancemanager.successfactors.eu/sf/liveprofile?_s.crb=GCk4JxCWiKUDD08a2WKBhKkyS0meHIE3sUS2RCjkXC4%3d",
-            Performance: "https://performancemanager.successfactors.eu/sf/pmreviews?bplte_company=saudiairliD&_s.crb=QC54RPnsfGg7EZLvxBP4qVoWXYP0Lm5WdB3I4ODG2GU%253d",
-            EmployeeProfile:"https://performancemanager.successfactors.eu/sf/liveprofile?_s.crb=GCk4JxCWiKUDD08a2WKBhKkyS0meHIE3sUS2RCjkXC4%3d",
-            OrganizationChart: "https://performancemanager.successfactors.eu/sf/orgchart?bplte_company=saudiairliD"
+            // Vacancy: "https://performancemanager.successfactors.eu/sf/careers/jobsearch?bplte_company=saudiairliD&_s.crb=3LsYZ05MZQCjDzCRxMwWTzwtWvMBA3IqTNRLTgCgQn4%253d",
+            // Learning: "https://performancemanager.successfactors.eu/sf/careers/jobsearch?bplte_company=saudiairliD&_s.crb=3LsYZ05MZQCjDzCRxMwWTzwtWvMBA3IqTNRLTgCgQn4%253d",
+            // Loan: "https://performancemanager.successfactors.eu/sf/liveprofile?_s.crb=GCk4JxCWiKUDD08a2WKBhKkyS0meHIE3sUS2RCjkXC4%3d",
+            // Performance: "https://performancemanager.successfactors.eu/sf/pmreviews?bplte_company=saudiairliD&_s.crb=QC54RPnsfGg7EZLvxBP4qVoWXYP0Lm5WdB3I4ODG2GU%253d",
+            // EmployeeProfile:"https://performancemanager.successfactors.eu/sf/liveprofile?_s.crb=GCk4JxCWiKUDD08a2WKBhKkyS0meHIE3sUS2RCjkXC4%3d",
+            // OrganizationChart: "https://performancemanager.successfactors.eu/sf/orgchart?bplte_company=saudiairliD"
             // *****************************************************************
 
             // ************************* UAT URL **************************
-            // Vacancy: "https://hcm2preview.sapsf.eu/sf/careers/jobsearch?bplte_company=saudiairliT1",
-            // Learning: "https://hcm2preview.sapsf.eu/xi/ui/peopleprofile/pages/index.xhtml?bplte_company=saudiairliT1",
-            // Loan: "https://hcm2preview.sapsf.eu/xi/ui/peopleprofile/pages/index.xhtml?bplte_company=saudiairliT1",
-            // Performance: "https://hcm2preview.sapsf.eu/sf/pmreviews?bplte_company=saudiairliT1",
-            // EmployeeProfile: "https://hcm2preview.sapsf.eu/xi/ui/peopleprofile/pages/index.xhtml?bplte_company=saudiairliT1",
-            // OrganizationChart: "https://hcm2preview.sapsf.eu/sf/orgchart?bplte_company=saudiairliT1"
+            Vacancy: "https://hcm2preview.sapsf.eu/sf/careers/jobsearch?bplte_company=saudiairliT1",
+            Learning: "https://hcm2preview.sapsf.eu/xi/ui/peopleprofile/pages/index.xhtml?bplte_company=saudiairliT1",
+            Loan: "https://hcm2preview.sapsf.eu/xi/ui/peopleprofile/pages/index.xhtml?bplte_company=saudiairliT1",
+            Performance: "https://hcm2preview.sapsf.eu/sf/pmreviews?bplte_company=saudiairliT1",
+            EmployeeProfile: "https://hcm2preview.sapsf.eu/xi/ui/peopleprofile/pages/index.xhtml?bplte_company=saudiairliT1",
+            OrganizationChart: "https://hcm2preview.sapsf.eu/sf/orgchart?bplte_company=saudiairliT1"
             // ************************* UAT URL **************************
 
         }
